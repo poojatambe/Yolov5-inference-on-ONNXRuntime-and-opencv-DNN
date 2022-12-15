7b131e9a51b5ae1ddb680de6086710978e70ef84
pooja <pooja123tambe@gmail.com>
2022-12-15 11:45:51 +0530

    source code

---
+++ yolov5_onnxinfer.py
@@ -0,0 +1,274 @@
+import random
+import numpy as np
+import onnxruntime as ort
+import cv2
+import time
+import torch
+import torchvision
+import argparse
+
+
+class ort_v5:
+    def __init__(self, img_path, onnx_model, conf_thres, iou_thres, img_size, cuda, classes):
+        self.img_path= img_path
+        self.onnx_model=onnx_model
+        self.conf_thres=conf_thres
+        self.iou_thres =iou_thres
+        self.img_size=img_size
+        self.cuda= cuda
+        self.names= classes
+
+    def __call__(self):
+
+        image_or= cv2.imread(self.img_path)
+        image, ratio, dwdh = self.letterbox(image_or, auto=False)
+        image = image.transpose((2, 0, 1))
+        image = np.expand_dims(image, 0)
+        image = np.ascontiguousarray(image)
+        im = image.astype(np.float32)
+        im /= 255
+        print(im.shape)
+
+        session= self.ort_session()
+        outname = [i.name for i in session.get_outputs()]
+        inname = [i.name for i in session.get_inputs()]
+        print('input-output names:',inname,outname)
+
+        inp = {inname[0]:im}
+        # ONNX inference
+        t1 = time.time()
+        outputs = session.run(outname, inp)[0]
+        output= torch.from_numpy(outputs)
+        out =self. non_max_suppression(output, conf_thres=0.7, iou_thres=0.5)[0]
+        print('predictions',out)
+        t2 = time.time()
+        print('onnxruntime inference time:', t2-t1)
+        img=self.result(image_or,ratio, dwdh, out)
+        cv2.imwrite('result.jpg', img)
+        # print('result', img.shape)
+        # cv2.imshow('result',img)
+        # cv2.waitKey(0)
+ 
+    def result(self,img,ratio, dwdh, out):
+        colors = {name:[random.randint(0, 255) for _ in range(3)] for i,name in enumerate(self.names)}   
+        for i,(x0,y0,x1,y1,score,cls_id) in enumerate(out):
+            box = np.array([x0,y0,x1,y1])
+            box -= np.array(dwdh*2)
+            box /= ratio
+            box = box.round().astype(np.int32).tolist()
+            cls_id = int(cls_id)
+            score = round(float(score),3)
+            name = self.names[cls_id]
+            color = colors[name]
+            name += ' '+str(score)
+            cv2.rectangle(img,box[:2],box[2:],color,2)
+            cv2.putText(img,name,(box[0], box[1] - 2),cv2.FONT_HERSHEY_SIMPLEX,0.75,[225, 255, 255],thickness=2) 
+        return img
+        # cv2.imwrite('v5_onnx.jpg', img) 
+        # cv2.imshow('result',img)
+        # cv2.waitKey(0)
+        
+    def ort_session(self):
+        providers = ['CUDAExecutionProvider', 'CPUExecutionProvider'] if self.cuda else ['CPUExecutionProvider']
+        session = ort.InferenceSession(self.onnx_model, providers=providers)
+        return session
+
+    def box_iou(self,box1, box2, eps=1e-7):
+        # https://github.com/pytorch/vision/blob/master/torchvision/ops/boxes.py
+        """
+        Return intersection-over-union (Jaccard index) of boxes.
+        Both sets of boxes are expected to be in (x1, y1, x2, y2) format.
+        Arguments:
+            box1 (Tensor[N, 4])
+            box2 (Tensor[M, 4])
+        Returns:
+            iou (Tensor[N, M]): the NxM matrix containing the pairwise
+                IoU values for every element in boxes1 and boxes2
+        """
+
+        # inter(N,M) = (rb(N,M,2) - lt(N,M,2)).clamp(0).prod(2)
+        (a1, a2), (b1, b2) = box1.unsqueeze(1).chunk(2, 2), box2.unsqueeze(0).chunk(2, 2)
+        inter = (torch.min(a2, b2) - torch.max(a1, b1)).clamp(0).prod(2)
+
+        # IoU = inter / (area1 + area2 - inter)
+        return inter / ((a2 - a1).prod(2) + (b2 - b1).prod(2) - inter + eps)
+
+    def non_max_suppression(self,
+            prediction,
+            conf_thres=0.7,
+            iou_thres=0.5,
+            classes=None,
+            agnostic=False,
+            multi_label=False,
+            labels=(),
+            max_det=300,
+            nm=0,  # number of masks
+    ):
+        """Non-Maximum Suppression (NMS) on inference results to reject overlapping detections
+
+        Returns:
+            list of detections, on (n,6) tensor per image [xyxy, conf, cls]
+        """
+
+        if isinstance(prediction, (list, tuple)):  # YOLOv5 model in validation model, output = (inference_out, loss_out)
+            prediction = prediction[0]  # select only inference output
+
+        device = prediction.device
+        mps = 'mps' in device.type  # Apple MPS
+        if mps:  # MPS not fully supported yet, convert tensors to CPU before NMS
+            prediction = prediction.cpu()
+        bs = prediction.shape[0]  # batch size
+        nc = prediction.shape[2] - nm - 5  # number of classes
+        xc = prediction[..., 4] > conf_thres  # candidates
+
+        # Checks
+        assert 0 <= conf_thres <= 1, f'Invalid Confidence threshold {conf_thres}, valid values are between 0.0 and 1.0'
+        assert 0 <= iou_thres <= 1, f'Invalid IoU {iou_thres}, valid values are between 0.0 and 1.0'
+
+        # Settings
+        # min_wh = 2  # (pixels) minimum box width and height
+        max_wh = 7680  # (pixels) maximum box width and height
+        max_nms = 30000  # maximum number of boxes into torchvision.ops.nms()
+        time_limit = 0.5 + 0.05 * bs  # seconds to quit after
+        redundant = True  # require redundant detections
+        multi_label &= nc > 1  # multiple labels per box (adds 0.5ms/img)
+        merge = False  # use merge-NMS
+
+        t = time.time()
+        mi = 5 + nc  # mask start index
+        output = [torch.zeros((0, 6 + nm), device=prediction.device)] * bs
+        for xi, x in enumerate(prediction):  # image index, image inference
+            # Apply constraints
+            # x[((x[..., 2:4] < min_wh) | (x[..., 2:4] > max_wh)).any(1), 4] = 0  # width-height
+            x = x[xc[xi]]  # confidence
+
+            # Cat apriori labels if autolabelling
+            if labels and len(labels[xi]):
+                lb = labels[xi]
+                v = torch.zeros((len(lb), nc + nm + 5), device=x.device)
+                v[:, :4] = lb[:, 1:5]  # box
+                v[:, 4] = 1.0  # conf
+                v[range(len(lb)), lb[:, 0].long() + 5] = 1.0  # cls
+                x = torch.cat((x, v), 0)
+
+            # If none remain process next image
+            if not x.shape[0]:
+                continue
+
+            # Compute conf
+            x[:, 5:] *= x[:, 4:5]  # conf = obj_conf * cls_conf
+
+            # Box/Mask
+            box = self.xywh2xyxy(x[:, :4])  # center_x, center_y, width, height) to (x1, y1, x2, y2)
+            mask = x[:, mi:]  # zero columns if no masks
+
+            # Detections matrix nx6 (xyxy, conf, cls)
+            if multi_label:
+                i, j = (x[:, 5:mi] > conf_thres).nonzero(as_tuple=False).T
+                x = torch.cat((box[i], x[i, 5 + j, None], j[:, None].float(), mask[i]), 1)
+            else:  # best class only
+                conf, j = x[:, 5:mi].max(1, keepdim=True)
+                x = torch.cat((box, conf, j.float(), mask), 1)[conf.view(-1) > conf_thres]
+
+            # Filter by class
+            if classes is not None:
+                x = x[(x[:, 5:6] == torch.tensor(classes, device=x.device)).any(1)]
+
+            # Apply finite constraint
+            # if not torch.isfinite(x).all():
+            #     x = x[torch.isfinite(x).all(1)]
+
+            # Check shape
+            n = x.shape[0]  # number of boxes
+            if not n:  # no boxes
+                continue
+            elif n > max_nms:  # excess boxes
+                x = x[x[:, 4].argsort(descending=True)[:max_nms]]  # sort by confidence
+            else:
+                x = x[x[:, 4].argsort(descending=True)]  # sort by confidence
+
+            # Batched NMS
+            c = x[:, 5:6] * (0 if agnostic else max_wh)  # classes
+            boxes, scores = x[:, :4] + c, x[:, 4]  # boxes (offset by class), scores
+            i = torchvision.ops.nms(boxes, scores, iou_thres)  # NMS
+            if i.shape[0] > max_det:  # limit detections
+                i = i[:max_det]
+            if merge and (1 < n < 3E3):  # Merge NMS (boxes merged using weighted mean)
+                # update boxes as boxes(i,4) = weights(i,n) * boxes(n,4)
+                iou = self.box_iou(boxes[i], boxes) > iou_thres  # iou matrix
+                weights = iou * scores[None]  # box weights
+                x[i, :4] = torch.mm(weights, x[:, :4]).float() / weights.sum(1, keepdim=True)  # merged boxes
+                if redundant:
+                    i = i[iou.sum(1) > 1]  # require redundancy
+
+            output[xi] = x[i]
+            if mps:
+                output[xi] = output[xi].to(device)
+            if (time.time() - t) > time_limit:
+                # LOGGER.warning(f'WARNING ⚠️ NMS time limit {time_limit:.3f}s exceeded')
+                break  # time limit exceeded
+
+        return output
+
+
+    def xywh2xyxy(self, x):
+        # Convert nx4 boxes from [x, y, w, h] to [x1, y1, x2, y2] where xy1=top-left, xy2=bottom-right
+        y = torch.zeros_like(x) if isinstance(x, torch.Tensor) else np.zeros_like(x)
+        y[:, 0] = x[:, 0] - x[:, 2] / 2  # top left x
+        y[:, 1] = x[:, 1] - x[:, 3] / 2  # top left y
+        y[:, 2] = x[:, 0] + x[:, 2] / 2  # bottom right x
+        y[:, 3] = x[:, 1] + x[:, 3] / 2  # bottom right y
+        return y
+
+
+    def letterbox(self, im, color=(114, 114, 114), auto=True, scaleup=True, stride=32):
+        # Resize and pad image while meeting stride-multiple constraints
+        shape = im.shape[:2]  # current shape [height, width]
+        new_shape= self.img_size
+        if isinstance(new_shape, int):
+            new_shape = (new_shape, new_shape)
+
+        # Scale ratio (new / old)
+        r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+        if not scaleup:  # only scale down, do not scale up (for better val mAP)
+            r = min(r, 1.0)
+
+        # Compute padding
+        new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
+        dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
+
+        if auto:  # minimum rectangle
+            dw, dh = np.mod(dw, stride), np.mod(dh, stride)  # wh padding
+
+        dw /= 2  # divide padding into 2 sides
+        dh /= 2
+
+        if shape[::-1] != new_unpad:  # resize
+            im = cv2.resize(im, new_unpad, interpolation=cv2.INTER_LINEAR)
+        top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
+        left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
+        im = cv2.copyMakeBorder(im, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
+        return im, r, (dw, dh)    
+            
+classes=['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 
+         'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 
+         'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 
+         'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 
+         'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 
+         'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 
+         'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 
+         'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 
+         'hair drier', 'toothbrush']
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--image',help='Specify input image', default= '', type=str)
+    parser.add_argument('--weights', default='./yolov5s.onnx', type=str, help='model weights path')
+    parser.add_argument('--conf_thres',default= 0.7, type=float, help='confidence threshold')
+    parser.add_argument('--iou_thres',type= float, default= 0.5, help='iou threshold')
+    parser.add_argument('--imgs', default=640,type= int, help='image size')
+    parser.add_argument('--cuda', default= False, help='define device')
+    # parser.add_argument('--classes', help='class names')
+    opt= parser.parse_args()
+    print(opt)
+    ORT= ort_v5(opt.image, opt.weights, conf_thres= opt.conf_thres, iou_thres=opt.iou_thres, img_size=(opt.imgs,opt.imgs), cuda=opt.cuda, classes=classes)
+    ORT()
